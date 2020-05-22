cc63f8cfd9af47cb4704fe5a8802a6ac354efe24
Addooo <marco.adobati@yahoo.it>
2020-05-22 19:37:20 +0200

    Homework 2

--- homework2/TEDX_watch_next.py
+++ homework2/TEDX_watch_next.py
@@ -1,7 +1,7 @@
 import sys
 import json # per lavorare json
 import pyspark #per lavare pyspark
-from pyspark.sql.functions import col, collect_list, array_join #alcune funzioni particolari di pyspark
+from pyspark.sql.functions import col, collect_list, concat, to_date, substring, lit, concat_ws, unix_timestamp #alcune funzioni particolari di pyspark
 
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
@@ -62,30 +62,39 @@ watch_dataset = spark.read.option("header","true").csv(watch_dataset_path)
 # CREATE THE AGGREGATE MODEL, ADD TAGS TO TEDX_DATASET
 tags_dataset_agg = tags_dataset.groupBy(col("idx").alias("idx_ref")).agg(collect_list("tag").alias("tags")) #raggruppo i dati, su cui posso fare una funzione di aggregazione
 tags_dataset_agg.printSchema() #per controllare lo schema per vedere se è giusto
-     #join fra dataset iniziale e quello dei tag
-      #perchè voglio che i miei dati siano indicizzati sul mio _id (e non che mongo si ricordi le sue cose)
+
+ #join fra dataset iniziale e quello dei tag
+ #perchè voglio che i miei dati siano indicizzati sul mio _id (e non che mongo si ricordi le sue cose)
 tedx_dataset_agg = tedx_dataset.join(tags_dataset_agg, tedx_dataset.idx == tags_dataset_agg.idx_ref, "left").drop("idx_ref").select(col("idx").alias("_id"), col("*")).drop("idx")
 tedx_dataset_agg.printSchema()
 
+       
+pattern = "dd-MMM-yyyy"
 
-watch_dataset_agg = watch_dataset.groupBy(col("idx").alias("idx_ref")).agg(collect_list("watch_next_idx"), collect_list("url"))
-tedx_dataset_watch = tedx_dataset_agg.join(watch_dataset_agg, tedx_dataset_agg.idx == watch_dataset_agg.idx_ref , "left").select(col("idx").alias("_id"), col("*")).drop("idx")
+#watch_dataset_agg = watch_dataset.groupBy(col("idx").alias("idx_ref")).agg(collect_list("watch_next_idx"), collect_list("url"))
+watch_dataset_agg = watch_dataset.groupBy(col("idx").alias("idx_ref")).agg(collect_list("watch_next_idx").alias("wnext"), collect_list("url").alias("url"))
+watch_dataset_agg.select(concat(col("wnext"),col("url")))
+tdw = tedx_dataset_agg.join(watch_dataset_agg, tedx_dataset_agg._id == watch_dataset_agg.idx_ref , "left").select(col("_id"), col("*")).drop("idx_ref")
+tdw2 = tdw.withColumn("day", lit("01"))
+tdw3 = tdw2.withColumn("data_app", concat_ws("-",tdw2.day, substring(tdw2.posted,8,3), substring(tdw2.posted,12,4)))
+tdw4 = tdw3.withColumn("data", unix_timestamp(tdw3.data_app, pattern).cast("timestamp"))
+tdw5 = tdw4.select(col("_id"), col("*")).drop("day").drop("posted").drop("data_app")
+tedx_dataset_watch_ord = tdw5.orderBy("data")
 
 #copio  i dati su MongoDB(imposto le opzioni)
 mongo_uri = "mongodb://clustercloudmobile-shard-00-00-x0zgo.mongodb.net:27017,clustercloudmobile-shard-00-01-x0zgo.mongodb.net:27017,clustercloudmobile-shard-00-02-x0zgo.mongodb.net:27017"
 
 write_mongo_options = {
-        "uri": mongo_uri,#indirizzo
-        "database": "unibg_tedx",#nome db
-        "collection": "tedz_data",#collezioni
-        "username": "XXXX",
-        "password": "XXXX",
-        "ssl": "true",
-         "ssl.domain_match": "false"
-         }
-          
+    "uri": mongo_uri,#indirizzo
+    "database": "unibg_tedx",#nome db
+    "collection": "tedz_data",#collezioni
+    "username": "XXXX",
+    "password": "XXXX",
+    "ssl": "true",
+    "ssl.domain_match": "false"}
+    
 from awsglue.dynamicframe import DynamicFrame
-tedx_dataset_dynamic_frame = DynamicFrame.fromDF(tedx_dataset_watch, glueContext, "nested")#converto il db glue in un spark (?)
+tedx_dataset_dynamic_frame = DynamicFrame.fromDF(tedx_dataset_watch_ord, glueContext, "nested")#converto il db glue in un spark (?)
 
 glueContext.write_dynamic_frame.from_options(tedx_dataset_dynamic_frame, connection_type="mongodb", connection_options=write_mongo_options)#vado a scrivere
 
